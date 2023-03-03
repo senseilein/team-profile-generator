eedf3efa4012997c6cebe3b313c5d7add98d84f8
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-03 23:56:34 +0000

    Create questions about team manager's info inside questions array

---
+++ starter/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    type: "input",
+    message: "Manager's name:",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "Manager's ID:",
+    name: "managerID",
+  },
+  {
+    type: "input",
+    message: "Manager's email address:",
+    name: "managerEmailAddress",
+  },
+  {
+    type: "input",
+    message: "Manager's office number:",
+    name: "managerOfficeNumber",
+  },
+];
