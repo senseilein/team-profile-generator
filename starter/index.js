a9b94611cbc9accd06bbad75981ec20c5b173550
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-04 00:24:02 +0000

    Create validation function to check input name/ID/email/officeNum in questions.js

--- starter/index.js
+++ starter/index.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-// const question = require("./lib/questions.js");
+const questions = require("./questions.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -14,5 +14,5 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 inquirer.prompt(questions).then((response) => {
-  console.log(data);
+  console.log(response);
 });
--- starter/questions.js
+++ starter/questions.js
@@ -1,22 +1,45 @@
+const isValidName = (name) => {
+  const letters = /^[a-z\s]*$/gi;
+  return name.trim().match(letters) ? true : "Invalid name!";
+};
+
+const isValidNum = (num) => {
+  return num.trim().match(/[^0-9]/g) ? "Invalid input" : true;
+};
+
+// regex to validate email address found at https://www.w3schools.blog/email-validation-javascript-js
+const isValidEmail = (email) => {
+  const mailformat =
+    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  return email.match(mailformat) ? true : "Invalid email address!";
+};
+
 const questions = [
   {
     type: "input",
     message: "Manager's name:",
     name: "managerName",
+    validate: isValidName,
   },
   {
     type: "input",
     message: "Manager's ID:",
     name: "managerID",
+    validate: isValidNum,
   },
   {
     type: "input",
     message: "Manager's email address:",
     name: "managerEmailAddress",
+    validate: isValidEmail,
   },
   {
     type: "input",
     message: "Manager's office number:",
     name: "managerOfficeNumber",
+    validate: isValidNum,
   },
 ];
+
+module.exports = questions;
