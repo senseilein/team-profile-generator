6a12d311b26f38c05294c5d9635bdcc24fbcdd6c
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-05 15:57:13 +0000

    Generate new team.html with updated html code (with updated margins, text-color/bg for employees cards)

--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -3,12 +3,12 @@ const generateTeam = (team) => {
   // creates the manager html
   const generateManager = (manager) => {
     return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        <div class="card employee-card m-3">
+        <div class="card-header bg-info">
+            <h2 class="card-title text-white">${manager.getName()}</h2>
+            <h3 class="card-title text-white"><i class="fas fa-mug-hot mr-2 text-warning"></i>${manager.getRole()}</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body bg-light">
             <ul class="list-group">
                 <li class="list-group-item">ID: ${manager.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
@@ -22,12 +22,12 @@ const generateTeam = (team) => {
   // creates the html for engineers
   const generateEngineer = (engineer) => {
     return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <div class="card employee-card m-3">
+    <div class="card-header bg-info">
+        <h2 class="card-title text-white">${engineer.getName()}</h2>
+        <h3 class="card-title text-white"><i class="fas fa-glasses mr-2 text-warning"></i>${engineer.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body bg-light">
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
@@ -41,12 +41,12 @@ const generateTeam = (team) => {
   // creates the html for interns
   const generateIntern = (intern) => {
     return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <div class="card employee-card m-3">
+    <div class="card-header bg-info">
+        <h2 class="card-title text-white">${intern.getName()}</h2>
+        <h3 class="card-title text-white"><i class="fas fa-user-graduate mr-2 text-warning"></i>${intern.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body bg-light">
         <ul class="list-group">
             <li class="list-group-item">ID: ${intern.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
@@ -101,13 +101,13 @@ module.exports = (team) => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center text-info">My Team</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="team-area col-12 d-flex justify-content-center flex-wrap">
                 ${generateTeam(team)}
             </div>
         </div>
